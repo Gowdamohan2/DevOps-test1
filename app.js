944862f196933d05ebfbacffa72241e62b443c48
Gowdamohan2 <gowdamohan221@gmail.com>
2025-08-04 14:47:15 +0530

    Files for app.js has been configured

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello from Node.js App with CI/CD!'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
